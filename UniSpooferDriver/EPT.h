75d3ce24051079e21d3bd1b3d9097dc609f89cfa
paciaronimarco10@gmail.com <88135556+BomboBombone@users.noreply.github.com>
2022-12-01 19:39:07 +0100

    feature(hypervisor): added checks for VMCS fail on VMEXIT

--- UniSpooferDriver/EPT.h
+++ UniSpooferDriver/EPT.h
@@ -9,7 +9,7 @@ typedef union _EPTP {
     struct {
         UINT64 MemoryType : 3; // bit 2:0 (0 = Uncacheable (UC) - 6 = Write - back(WB))
         UINT64 PageWalkLength : 3; // bit 5:3 (This value is 1 less than the EPT page-walk length) 
-        UINT64 DirtyAndAceessEnabled : 1; // bit 6  (Setting this control to 1 enables accessed and dirty flags for EPT)
+        UINT64 DirtyAndAccessEnabled : 1; // bit 6  (Setting this control to 1 enables accessed and dirty flags for EPT)
         UINT64 Reserved1 : 5; // bit 11:7 
         UINT64 PML4Address : 36;
         UINT64 Reserved2 : 16;
